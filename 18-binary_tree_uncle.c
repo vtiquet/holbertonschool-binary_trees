04fa7630bba129dcfb800e62f673b85f013351c7
vtiquet <valentin.tiquet@gmail.com>
2025-08-06 13:25:42 +0000

    Updated to comply with Betty coding style

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,11 +10,13 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	if ((*node).parent->parent->left == (*node).parent && (*node).parent->parent->right)
+	if ((*node).parent->parent->left == (*node).parent
+		&& (*node).parent->parent->right)
 	{
 		return ((*node).parent->parent->right);
 	}
-	if ((*node).parent->parent->right == (*node).parent && (*node).parent->parent->left)
+	if ((*node).parent->parent->right == (*node).parent
+		&& (*node).parent->parent->left)
 	{
 		return ((*node).parent->parent->left);
 	}
