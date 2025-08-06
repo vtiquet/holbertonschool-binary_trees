e1fc020dd4808f069a3fd4bb6348f359fd399763
vtiquet <valentin.tiquet@gmail.com>
2025-08-06 13:21:41 +0000

    Fixed code

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,9 +6,17 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || (*node).parent == NULL)
+	if (node == NULL || (*node).parent == NULL || (*node).parent->parent == NULL)
 	{
 		return (NULL);
 	}
-	return (binary_tree_sibling((*node).parent));
+	if ((*node).parent->parent->left == (*node).parent && (*node).parent->parent->right)
+	{
+		return ((*node).parent->parent->right);
+	}
+	if ((*node).parent->parent->right == (*node).parent && (*node).parent->parent->left)
+	{
+		return ((*node).parent->parent->left);
+	}
+	return (0);
 }
